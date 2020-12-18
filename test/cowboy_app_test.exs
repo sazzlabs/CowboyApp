6494b7260a4af9f423e439b23abdb93449b190e5
Sazzo <felipe@sazz.fail>
2020-12-18 11:57:38 -0300

    Update tests

--- test/cowboy_app_test.exs
+++ test/cowboy_app_test.exs
@@ -1,8 +1,17 @@
 defmodule CowboyAppTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest CowboyApp
 
-  test "greets the world" do
-    assert CowboyApp.hello() == :world
+  use Plug.Test
+  @opts CowboyApp.Endpoint.init([])
+
+  test "it returns hello world" do
+    conn = conn(:get, "/")
+
+    conn = CowboyApp.Endpoint.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Hello World!"
   end
 end
